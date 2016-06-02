5a10f0249d1749b014abc0c6238376ce2a071a77
ies <ies>
2016-06-02 16:30:30 +0200

    Arxiu Java documentat, amb l'explicació de cada funció

--- Main_dolent.java
+++ Main_dolent.java
@@ -1,14 +1,30 @@
-package com.company;
+/**
+ * @Author Xavi Molar
+ * Examen DAM JavaDOC
+ * @version 1.0
+ */
 
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
-
+public class Main_dolent {
+	
+	/**
+	 * Declaració de les variables, on tenim un String paraules amb les possibles respostes correctes del joc,
+	 * un integer index i un altre String "p"
+	 *
+	 */
+	
     static protected String paraules[] = {"hola","programaciÃ³","casa"};
     static protected int index;
     static protected String p;
-
+    
+    
+    /**
+     * S'inicia el joc i s'estableix la condició de que es podrá jugar mentres el numero de errors no sigui superior al numero de
+     * lletres que estem buscant
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("ComenÃ§a el joc!");
 
@@ -31,7 +47,14 @@ public class Main {
             System.out.println(p);
         }
    }
-
+    
+    
+    
+    /**
+     * La funció principal aqui es demanar una nova lletra, tant si la introduida forma part de la paraula que estem buscant com si no
+     * @param nova
+     * @return
+     */
     static public boolean nova_lletra(char nova) {
         boolean trobada = false;
         String paraula_old = p;
@@ -47,6 +70,9 @@ public class Main {
         }
         return trobada;
     }
+    
+    /**
+     * Fi del programa.
+     */
 
 }
-
