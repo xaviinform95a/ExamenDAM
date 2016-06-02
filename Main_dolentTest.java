f82a9cfe1fd3ca0f1a590066244fd837b368edb7
ies <ies>
2016-06-02 17:01:59 +0200

    Afegim el JUnit del programa Main_dolent.java

---
+++ Main_dolentTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Main_dolentTest {
+
+	@Test
+	public void testMain() {
+		assertEquals("hola", 3, );
+	}
+
+	@Test
+	public void testNova_lletra() {
+		fail("Not yet implemented");
+	}
+
+}
